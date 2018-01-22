8e224b2ae4f71cfe6131b791e783d5eda2b2aafa
nemunemu <e155719@ie.u-ryukyu.ac.jp>
2018-01-22 20:01:23 +0900

    Work In Progress

--- graphql.go
+++ graphql.go
@@ -3,14 +3,27 @@ package main
 import (
   "fmt"
   "net/http"
+  "net/url"
+  "strings"
 
   "github.com/antonholmquist/jason"
 )
 
 func main() {
-  url := "https://api.github.com/graphql"
+  URL := "https://api.github.com/graphql"
+  values := url.Values{}
+  //values := url.Values{`{repository(owner: "octocat", name: "Hello-World") {issues(last: 20, states: CLOSED) {edges {node {title url labels(first: 5) { edges { node { name}}}}}}}}`}
 
-  req, err := http.NewRequest("POST", url, nil)
+  // v3のapiを叩かれるっぽいから違うと思われる -----------------------------
+  // values := url.Values{}
+  // values.Add("user", "nemunemuryo")
+  // -----------------------------------------------------------------------
+
+
+  body := strings.NewReader(values.Encode())
+
+  // 第3引数がbodyらしいからここだと思う...
+  req, err := http.NewRequest("POST", URL, body)
   req.Header.Set("Authorization", "bearer <your token>")
 
   if err != nil {
@@ -27,13 +40,13 @@ func main() {
 
   v, err := jason.NewObjectFromReader(resp.Body)
 
-  fmt.Print(v, "\n\n")
+  fmt.Print(v, "\n")
 
   if err != nil {
     panic(err)
   }
 
-  name, err:= v.GetString("repository")
-  fmt.Print(name)
+  errors, err:= v.GetObjectArray("errors")
+  fmt.Print(errors)
 
 }
